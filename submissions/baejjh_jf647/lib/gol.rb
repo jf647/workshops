1698054886f7c7ededa7d233d7879bc6c12b7eab
James FitzGibbon <james.i.fitzgibbon@nordstrom.com>
2015-03-03 20:07:23 -0800

    fix test, add new test

--- submissions/baejjh_jf647/lib/gol.rb
+++ submissions/baejjh_jf647/lib/gol.rb
@@ -1,5 +1,35 @@
 class GOL
+	def initialize(size = 20)
+		@board = []
+		1.upto(size) do |x|
+			row = []
+			1.upto(size) do |y|
+				row.push false
+			end
+			@board.push row
+		end
+	end
+
 	def num_cells
 		0
+	end
+
+	def evolve!
+		new_board = evolve
+		@board = new_board
+	end
+
+	private
+
+	def evolve
+		@board.map do |row|
+			row.map do |cell|
+				evolve_cell(cell)
+			end
+		end
+	end
+
+	def evolve_cell(cell)
+		cell
 	end
 end
--- submissions/baejjh_jf647/spec/lib/gol_spec.rb
+++ submissions/baejjh_jf647/spec/lib/gol_spec.rb
@@ -13,6 +13,12 @@ RSpec.describe GOL do
   		game.evolve!
   		expect(game.num_cells).to be_zero
   	end
+
+    it 'a single cell dies after one evolution' do
+      game = GOL.new
+      game.set_alive(0, 0)
+      game.evolve!
+      expect(game.num_alive_cells).to be_zero
+    end
   end
-  
 end
